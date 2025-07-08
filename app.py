4c8510fdd706a94b60a8c82517677781c516087c
Marcos Vinicius Sousa De Carvalho <mvcarvalho@bbts.com.br>
2025-07-08 10:36:48 -0300

    update app.py & test_task.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ task_id_control = 1
 def create_tasks():
     global task_id_control
     data = request.get_json()
+    new_task = Task(id=task_id_control, title=data["title"], description=data.get("description"), completed=False)
     new = {
         "id": task_id_control,
         "title": data.get("title"),
@@ -18,7 +20,7 @@ def create_tasks():
     tasks.append(new)
     task_id_control += 1
     print(tasks)
-    return jsonify({"message": "Nova tarefa criada com sucesso"}), 201
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id}), 201
 
 @app.route("/tasks", methods=['GET'])
 def get_tasks():
